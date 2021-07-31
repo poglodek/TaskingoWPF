5b6bdf1e922a3d390f09900f00c58c58caba1275
poglodek <pawelogll@gmail.com>
2021-07-31 20:24:01 +0200

    adding view to login

--- TaskingoApp/MainWindow.xaml.cs
+++ TaskingoApp/MainWindow.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace TaskingoApp
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LoginScreen.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginScreen : Window
     {
-        public MainWindow()
+        public LoginScreen()
         {
             InitializeComponent();
         }
---
+++ TaskingoApp/View/LoginScreen.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace TaskingoApp.ViewModel
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}

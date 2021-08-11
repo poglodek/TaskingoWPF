ee744b15de2206b64125543db7e4b0a154349d0d
poglodek <pawelogll@gmail.com>
2021-08-11 18:45:16 +0200

    Refactor BaseCall class

--- TaskingoApp/APICall/BaseCall.cs
+++ TaskingoApp/APICall/BaseCall.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -16,12 +17,24 @@ namespace TaskingoApp.APICall
 
         public static async Task<string> MakeCall(string endpoint, HttpMethod httpMethod, object body)
         {
+            var respone = await CallApi(endpoint, httpMethod, body);
+            return await CheckResponse(respone);
+        }
+
+        private static async Task<HttpResponseMessage> CallApi(string endpoint, HttpMethod httpMethod, object body)
+        {
             using var httpClient = new HttpClient();
             var request = new HttpRequestMessage();
             request.Method = HttpMethod.Put;
             request.RequestUri = new Uri(Url + endpoint);
             request.Content = new StringContent(JsonSerializer.Serialize(body));
+            if(!string.IsNullOrEmpty(Token)) httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Token);
             var respone = await httpClient.SendAsync(request);
+            return respone;
+        }
+
+        private static async Task<string> CheckResponse(HttpResponseMessage respone)
+        {
             if (respone.IsSuccessStatusCode)
             {
                 var odp = await respone.Content.ReadAsStringAsync();

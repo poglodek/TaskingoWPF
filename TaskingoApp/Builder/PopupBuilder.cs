82d759d7812498585f6e3763e54f867150e8c8f8
poglodek <pawelogll@gmail.com>
2021-08-21 16:25:45 +0200

    Repair bugs

---
+++ TaskingoApp/Builder/PopupBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using TaskingoApp.View;
+
+namespace TaskingoApp.Builder
+{
+    public class PopupBuilder
+    {
+        public static void Build(string errorMessage)
+        {
+            
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                var info = new InfoPopupView { Message = { Text = errorMessage } };
+                info.Show();
+            });
+            
+        }
+    }
+}
---
+++ TaskingoApp/View/InfoPopupView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace TaskingoApp.View
+{
+    /// <summary>
+    /// Interaction logic for InfoPopupView.xaml
+    /// </summary>
+    public partial class InfoPopupView : Window
+    {
+        public InfoPopupView()
+        {
+            InitializeComponent();
+        }
+    }
+}
